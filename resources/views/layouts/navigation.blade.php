9521c2099d5c6a2e4514e3e9ff09987c1e760291
alcigon <alcigon@gmail.com>
2024-09-19 16:19:21 -0400

    2024-09-19

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,7 @@
+@if (session('status'))
+    <div class="bg-green-600 text-green-100 text-center text-lg font-bold p-2" style="background-color:brown">{{ session('status') }}</div>
+@endif
+
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -14,6 +18,9 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                        {{ __('chirps') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -69,6 +76,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                {{ __('Chirps') }}
             </x-responsive-nav-link>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirpController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,19 +15,52 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
+Route::view('/', 'welcome')->name('welcome');
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+*/
 
 Route::middleware('auth')->group(function () {
+
+    Route::view('/dashboard','dashboard')->name('dashboard');
+        
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/chirps', [ChirpController::class, 'index'])->name('chirps.index');
+
+    Route::post('/chirps', [ChirpController::class,'store'])->name('chirps.store');
+    
+    //session()->flash('status', 'Chirp creado con éxito!');  // session flash message
+
+    /*
+    Route::get('/chirps', function () {
+        return view('chirps.index');
+    })->name('chirps.index');
+    */
+
+    /*
+    Route::post('/chirps', function () {
+        
+        Chirp::create([
+            'message' => request('message'),
+            'user_id' => auth()->id(),
+        ]);
+
+        //session()->flash('status', 'Chirp creado con éxito!');  // session flash message
+
+        return to_route('chirps.index')->with('status', 'Chirp creado con éxito!');  // session flash message;
+        
+    });*/
 });
 
 require __DIR__.'/auth.php';
